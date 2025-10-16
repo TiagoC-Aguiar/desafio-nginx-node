b54642382931ede19bc200180b79a4aa71cfd6f8
Tiago Aguiar <tyago_tka48@hotmail.com>
2025-10-16 22:58:27 +0100

    chore: use typescript

--- node/src/index.ts
+++ node/src/index.ts
@@ -1,8 +1,9 @@
-const express = require('express');
-const axios = require('axios').default;
-const mysql = require('mysql');
+import express from 'express';
+import axios from 'axios';
+import mysql from 'mysql';
+import app from './server.js';
 
-const app = express();
+// const app = express();
 const PORT = 3000;
 
 const config = {
@@ -23,36 +24,36 @@ app.listen(PORT, () => {
 async function getPersonName() {
   const RANDOM = Math.floor(Math.random() * 10);
   const response = await axios.get('https://swapi.dev/api/people');
-  personName = response.data.results;
+  const personName = response.data.results;
   return personName[RANDOM].name;
 }
 
-async function insertPeopleName(res) {
+async function insertPeopleName(res: any) {
   const name = await getPersonName();
   const connection = mysql.createConnection(config);
   const sql = `INSERT INTO people(name) values('${name}')`;
-    
+
   connection.query(sql);
-  console.log(`${name} inserido no banco!`);    
+  console.log(`${name} inserido no banco!`);
   getPeople(res, connection);
 }
 
-function getPeople(res, connection) {    
-  const sql = `SELECT id, name FROM people`;  
-  
-  connection.query(sql, (error, results, fields) => {
+function getPeople(res: any, connection: any) {
+  const sql = `SELECT id, name FROM people`;
+
+  connection.query(sql, (error: any, results: any, fields: any) => {
     if (error) {
       throw error
     };
-    
+
     let table = '<table>';
     table += '<tr><th>#</th><th>Name</th></tr>';
-    for(let people of results) {      
+    for (let people of results) {
       table += `<tr><td>${people.id}</td><td>${people.name}</td></tr>`;
     }
 
-    table += '</table>';    
-    res.send('<h1>Full Cycle Rocks!</h1>' + table);    
-  });   
+    table += '</table>';
+    res.send('<h1>Full Cycle Rocks!</h1>' + table);
+  });
   connection.end();
 }
