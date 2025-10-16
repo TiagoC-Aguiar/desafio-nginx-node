6c9ced2d929409aaaac1a497aa43d3a11e22dd0f
Tiago Aguiar <tyago_tka48@hotmail.com>
2025-10-16 21:24:15 +0100

    chore: atualiza mysql

--- node/.docker/mysql/initdb.sql
+++ node/.docker/mysql/initdb.sql
@@ -1,3 +1,7 @@
+-- fix Error: ER_NOT_SUPPORTED_AUTH_MODE: Client does not support authentication protocol requested by server; consider upgrading MySQL client
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
+FLUSH PRIVILEGES;
+
 CREATE DATABASE IF NOT EXISTS nodedb;
 CREATE TABLE IF NOT EXISTS `nodedb`.`people` (
   `id` INT AUTO_INCREMENT PRIMARY KEY,
